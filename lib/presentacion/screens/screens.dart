243bd6a3e605b6bbb99381723a840ce8e8b86334
Ricardo7173 <riki0303@outlook.es>
2024-06-27 17:31:26 -0600

    Configuración de pantallas restantes

--- lib/presentacion/screens/screens.dart
+++ lib/presentacion/screens/screens.dart
@@ -1,3 +1,9 @@
+export 'package:orgg_widget_app/presentacion/screens/animated/animated_screen.dart';
+export 'package:orgg_widget_app/presentacion/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:orgg_widget_app/presentacion/screens/buttons/button_screen.dart';
 export 'package:orgg_widget_app/presentacion/screens/card/cards_screen.dart';
 export 'package:orgg_widget_app/presentacion/screens/home/home_screens.dart';
+export 'package:orgg_widget_app/presentacion/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:orgg_widget_app/presentacion/screens/progress/progress_screen.dart';
+export 'package:orgg_widget_app/presentacion/screens/snackbar/snackbar_screen.dart';
+export 'package:orgg_widget_app/presentacion/screens/ui_controls/ui_controls_screen.dart';
